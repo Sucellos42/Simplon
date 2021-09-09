329dda7b2047a49a03d4f17d94b238c52affa61c
leo.recouvreux <leorecouvreux@ThinkPad-T430-0ddd436f.localdomain>
2021-09-09 17:34:53 +0200

    commit exo 16

--- exos-leo/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo "<h1>" . get_current_user() . "</h1>";
-echo "<p>" . date(" d M Y") . "</p>"; ?>
--- exos-leo/pages/page-16.php
+++ exos-leo/pages/page-16.php
@@ -2,17 +2,15 @@
 
 echo('<h1> ** Sommaire ** </h1>');
 
+
 $folder = "../pages";
-$files = scandir($folder);
+$files = new arrayIterator(scandir($folder));
+
+$folder_regx = new RegexIterator($files, '/page\-1[0 -5]/');
 
 
-// unset($files[array_search('poke-plante.txt', $files)]);
-// unset($files[array_search('poke-feu.txt', $files)]);
-// unset($files[array_search('poke-eau.txt', $files)]);
-// unset($files[array_search('citations.txt', $files)]);
-print_r($files);
 
 
-foreach($files as $file){
-    echo ("<li><a href=$file'>$file</a></li>");
+foreach($folder_regx as $file){
+    echo ("<li><a href=$file>$file</a></li>");
 }
