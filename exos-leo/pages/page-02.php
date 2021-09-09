48f508e764527eb2f967bb0e0ea352126caef8e0
Sucellos42 <88437007+Sucellos42@users.noreply.github.com>
2021-09-09 18:23:46 +0200

    Delete exos-leo directory

--- exos-leo/pages/page-02.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define('NL', '<br/>');
-
-for ($i = 1; $i <= 100; $i++) {
-    echo("Phrase n° $i :  " . NL .  "C'est une phrase je crois. " . NL . NL );
-}
--- exos-leo/tuto_boostrap/contact.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require 'header.php';
-?>
-
-<h2>Contact</h2>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam odit laboriosam quaerat laudantium voluptatum quod harum ut nemo doloribus repudiandae aperiam expedita modi dolorum numquam, nostrum aliquid alias ad amet eligendi soluta!</p>
